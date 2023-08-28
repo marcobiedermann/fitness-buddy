97f83d1ea07bd9d32598350570f552cc8051bc5c
Marco Biedermann <5244986+marcobiedermann@users.noreply.github.com>
2023-08-28 17:37:29 +0200

    fix(web): Callback URL
    Set auth callback URL via environment variable

--- apps/web/src/app/process.env.d.ts
+++ apps/web/src/app/process.env.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    NEXT_PUBLIC_AUTH_CALLBACK_URL: string;
     NEXT_PUBLIC_SUPABASE_URL: string;
     NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
   }
